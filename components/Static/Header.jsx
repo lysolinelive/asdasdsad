4cb9023f1eb706fbd01b1b54749a148e19840a0e
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 15:31:55 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -76,7 +76,7 @@ const Header = ({ $, NavItems }) => {
                 <div className="max-w-7xl px-5 mx-auto py-5 flex items-center justify-between">
                     <div className="flex items-center space-x-6">
                         <div className="flex items-center space-x-3">
-                            <img src="/img/logo2.png" 
+                            <img src="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" 
                                 className={`
                                     rounded-full 
                                 `}
