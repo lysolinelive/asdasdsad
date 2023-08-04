ff9bedf05244b8dfa1c00268b0b674947726af98
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 16:17:59 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,7 +13,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="/img/logo.jpg" />
+                        <img width="32" className="rounded-full icon-glow" src="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" />
                         <p className=" font-extrabold ml-1 text-2xl">Lysoline Bot</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
