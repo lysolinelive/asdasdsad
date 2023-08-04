a21b4cbcb42a89f5787e9b07e339e64b7e288e53
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 16:41:48 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,13 +6,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" href="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" />
-           <meta name="description" content="Lysoline Bot İnvite." />
-    <meta property="og:description" content="Lysoline Bot İnvite." />
-    <meta property="og:image" content="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" />
-    <meta property="og:image:alt" content="Lysoline" />
-    <meta property="og:site_name" content="Lysoline" />
-
+         
+                <title>Discord Bot Dokümantasyonu</title>
+                <meta name="description" content="Lysoline Bot İnvite." />
+                <meta property="og:description" content="Lysoline Bot İnvite." />
+                <meta property="og:image" content="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" />
+                <meta property="og:image:alt" content="Lysoline" />
+                <meta property="og:site_name" content="Lysoline" />
+                {/* Diğer head içeriği */}
+           
         </Head>
         <body>
           <Main />
