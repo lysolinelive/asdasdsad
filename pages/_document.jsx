96b0711753a3e6d7db77fcb398a119a7e1debbd0
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 16:32:49 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,8 +7,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="apple-touch-icon" href="https://cdn.discordapp.com/attachments/1014356400358572052/1136999773388419072/Lysolineavatar.png" />
-          <meta name="robots" content="index,follow" />
-          <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
             content="Lysoline BOT İnvite "
